aad0935ca388dd9915051dce0e34f65f01835882
Dalton Jens <126439073+DaltonJens@users.noreply.github.com>
2023-05-08 17:57:01 +0800

    Start_window Update
    Changed the Center script to function correctly. Del of size config and made it a global variable

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ MAIN_RED = "#D73c37"
 SOFT_RED = "#EE6146"
 DEEP_YELLOW = "#DFBC5E"
 SOFT_YELLOW = "#E6E0AE"
+SW_WIDTH = 750   # Start Window Width
+SW_HEIGHT = 400   # Start Window Height
 
 # Get screen width and height
 screen = screeninfo.get_monitors()[0]
@@ -18,9 +20,9 @@ screen_height = screen.height
 start_window = Tk()
 start_window.wm_title("Flashyee Cards")    # Window's titles
 start_window.wm_iconbitmap('images\zhong_favicon.ico')    # Favicon
-start_window.wm_geometry("800x600")   # Size of the window at startup
-start_window.wm_minsize(width=750, height=400)   # Minimum size of window when compressed
+start_window.wm_minsize(width=750, height=400)   # Min/Max size of window when compressed
 start_window.wm_maxsize(width=750, height=400)
+
 start_window.resizable(False, False)    # Not resizable in (Width,Height)
 start_window.config(padx=100, pady=200)   # Pading around the boarder 
 
@@ -28,10 +30,27 @@ start_window.config(padx=100, pady=200)   # Pading around the boarder
 start_window.attributes("-topmost", True)
 start_window.attributes("-topmost", False)
 
-# Possible centering code, doesnt work well with the window startup geometry
-x = (screen_width - start_window.winfo_reqwidth()) // 2
-y = (screen_height - start_window.winfo_reqheight()) // 2
-start_window.geometry("+{}+{}".format(x, y))
+# Centers start window
+x = (screen_width - SW_WIDTH) // 2
+y = (screen_height - SW_HEIGHT) // 2
+start_window.wm_geometry("+{}+{}".format(x, y))
+
+# Coloring Format
+start_window.config(bg=SOFT_YELLOW)
+
+class Window():
+    pass
+
+
+
+
+
+
+
+
+
+
+
 
 
 start_window.mainloop()
